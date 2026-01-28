c679aa747880eab90a7eaf26293aa3ee13a4dfb8
gilltran <can.tran@gogx.com>
2026-01-28 23:03:01 +0700

    initial setting

---
+++ src/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const configViewEngine = require('./config/viewEngine');
+const app = express()
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+const webRouter = require('./routes/web');
+
+//config template
+configViewEngine(app);
+
+//declare router
+
+app.use('/', webRouter);
+
+
+app.listen(port, hostname, () => {
+    console.log("Server running");
+});
